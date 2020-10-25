f98158c8bc3826e74d54b8145e987a9f5f063b73
fenya007 <59559796+fenya007@users.noreply.github.com>
2020-10-25 18:36:29 +0300

    docs:add task bracke

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = str;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    for (let k = 0; k < str.length; k++){
+      if (str[k] == bracketsConfig[i][0] && str[k + 1] == bracketsConfig[i][1]) {
+        res = str.replace(bracketsConfig[i][0] + bracketsConfig[i][1], '')
+      }  
+    }
+  }
+  return res == str ? res == '' : check(res,bracketsConfig)
 }
